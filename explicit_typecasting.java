06c9f3cdd4df0477aba0caf6a16b94172cba9ee0
jyotish kumar <jyotishkumarjhar@gmail.com>
2026-06-12 23:05:23 +0530

    java

---
+++ explicit_typecasting.java
@@ -0,0 +1,8 @@
+public class  explicit_typecasting {
+    public static void main(String[] args) {
+        double num = 99.99;
+        int value = (int) num;
+
+        System.out.println(value);
+    }
+}
---
+++ implicit_typecasting.java
@@ -0,0 +1,8 @@
+public class implicit_typecasting {
+    public static void main(String[] args) {
+        int num = 100;
+        double value = num;
+
+        System.out.println(value);
+    }
+}
